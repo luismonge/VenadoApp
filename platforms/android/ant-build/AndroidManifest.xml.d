5ca1fa7154f5e94e12532d4853bffe5dd08370d5
Luis Carlos <luis_carlos_09@hotmail.com>
2014-11-29 19:04:39 -0700

    First

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\ant-build\classes.dex : \
+C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\ant-build\classes\org\venado\app\BuildConfig.class \
+C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\ant-build\classes\org\venado\app\R$attr.class \
+C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\ant-build\classes\org\venado\app\R$drawable.class \
+C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\ant-build\classes\org\venado\app\R$string.class \
+C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\ant-build\classes\org\venado\app\R$xml.class \
+C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\ant-build\classes\org\venado\app\R.class \
+C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\ant-build\classes\org\venado\app\VenadoApp.class \
+C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\ant-build\dexedLibs\classes-994c686194cdad490d36309780ca0e9c.jar \
