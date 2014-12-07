6ed5f5d54b87ef1303c1b5599fcc67b894b51f0e
Luis Monge <luis_carlos_09@hotmail.com>
2014-12-07 13:33:51 -0700

    rm

--- platforms/android/ant-build/VenadoApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\ant-build\VenadoApp-debug-unaligned.apk : \
-C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\ant-build\VenadoApp.ap_ \
-C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\ant-build\classes.dex \
-C:\Users\LuisCarlos\Documents\EclipseProjects\VenadoApp\platforms\android\CordovaLib\ant-build\classes.jar \
