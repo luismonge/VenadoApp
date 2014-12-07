7e2ee9557a36937e6c07e046621e43bf36c5ac36
Luis Monge <luis_carlos_09@hotmail.com>
2014-12-07 13:43:26 -0700

    haber que pasa

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/luis/Documentos/Development/VenadoApp/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/luis/Documentos/Development/VenadoApp/platforms/android/CordovaLib/AndroidManifest.xml \
