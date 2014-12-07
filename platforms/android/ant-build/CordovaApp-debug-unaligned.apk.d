cfe1ad0436f8650b36dabc40ba830b17dd922ebf
Luis Monge <luis_carlos_09@hotmail.com>
2014-12-07 12:48:01 -0700

    Subida desde linux

---
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,5 @@
+/home/luis/Documentos/Development/VenadoApp/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
+/home/luis/Documentos/Development/VenadoApp/platforms/android/ant-build/CordovaApp.ap_ \
+/home/luis/Documentos/Development/VenadoApp/platforms/android/ant-build/classes.dex \
+/home/luis/Documentos/Development/VenadoApp/platforms/android/CordovaLib/ant-build/classes.jar \
+/home/luis/Documentos/Development/VenadoApp/platforms/android/libs/Pushwoosh.jar \
