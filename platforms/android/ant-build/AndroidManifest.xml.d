7f672b99bc4e75cc4dc265e18a4ff971b6c4f751
Luis Monge <luis_carlos_09@hotmail.com>
2014-12-07 15:36:50 -0700

    haber

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/luis/Documentos/Development/VenadoApp/platforms/android/ant-build/AndroidManifest.xml : \
-/home/luis/Documentos/Development/VenadoApp/platforms/android/AndroidManifest.xml \
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/luis/Documentos/Development/VenadoApp/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/home/luis/Documentos/Development/VenadoApp/platforms/android/ant-build/CordovaApp.ap_ \
-/home/luis/Documentos/Development/VenadoApp/platforms/android/ant-build/classes.dex \
-/home/luis/Documentos/Development/VenadoApp/platforms/android/CordovaLib/ant-build/classes.jar \
